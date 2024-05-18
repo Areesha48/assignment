1d70fe561b0594b6574436fee020bad8154318f0
Areesha shehzad <157312844+Areesha48@users.noreply.github.com>
2024-05-18 23:15:42 +0500

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,42 @@
+//chap2//
+//question 1//
+let username;
+
+//question 2//
+let myName ='bushra';
+
+//question 3//
+let message = "Hello world";
+alert(message);
+
+//question 4//
+let name = "John doe";
+let year = "15";
+let gender = "male";
+let certified = "certified mobile app development";
+
+alert("John doe" );
+alert("15 years old" );
+alert("male" );
+alert("certified mobile app Development" );
+
+//question 5//
+let pizzaAlert = "Pizza \npizz \npiz \npi \np";
+alert(pizzaAlert);
+
+//question 6//
+let email = "my email is bushrashehzad@90gmail.com";
+alert (email);
+
+
+//question 7//
+let book = "A smarter way to learn Javascript";
+alert("I am trying to learn from the Book " + book);
+
+//Question 8//
+document .write("Yah! i can write HTML content through javascript");
+
+//Question 9//
+let sticker = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert( sticker);
+  
